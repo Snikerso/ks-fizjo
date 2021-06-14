73e5f4f1116cb19fdc639b5c7c4cf58cc419be33
Snikerso <42245963+Snikerso@users.noreply.github.com>
2021-06-14 19:08:46 +0200

    messenger

--- pages/_document.js
+++ pages/_document.js
@@ -29,35 +29,47 @@ class MyDocument extends Document {
 
 
   render() {
+    <!-- Messenger Wtyczka czatu Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Wtyczka czatu code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
 
 
     return (
       <Html>
         <Head>
             <>
-
+            <div id="fb-root"></div>
             <script dangerouslySetInnerHTML={{
                         __html: `
+                        var chatbox = document.getElementById('fb-customer-chat');
+                        chatbox.setAttribute("page_id", "1626967450920148");
+                        chatbox.setAttribute("attribution", "biz_inbox");
                         window.fbAsyncInit = function() {
-                            FB.init({
-                              xfbml            : true,
-                              version          : 'v10.0'
-                            })
-                          };
+                          FB.init({
+                            xfbml            : true,
+                            version          : 'v11.0'
+                          });
+                        };
+                  
                         (function(d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (d.getElementById(id)) return;
-                            js = d.createElement(s); js.id = id;
-                        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-                        fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));`
+                          var js, fjs = d.getElementsByTagName(s)[0];
+                          if (d.getElementById(id)) return;
+                          js = d.createElement(s); js.id = id;
+                          js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js';
+                          fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));
+                      `
                     }} />
                 <div id="fb-customer-chat" className="fb-customerchat"></div>
                 <title>lalalla</title>
             </>
         </Head>
         <body>
-            <div id="fb-root"></div>
+            
             <Main />
             <NextScript />
         </body>
